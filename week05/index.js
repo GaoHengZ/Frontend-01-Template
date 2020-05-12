35d183c5a540c1cfc3e69d0b841b548f6aba0c13
冰已凋零 <869138304@qq.com>
2020-05-12 21:16:44 +0800

    HttpRequest

---
+++ week05/index.js
@@ -0,0 +1,16 @@
+const HttpRequest = require("./Request");
+
+const request = new HttpRequest({
+	port : "8880",
+	host : "127.0.0.1",
+	path : "/getData",
+	method : "GET",
+	body : {
+		name : "custom"
+	}
+});
+request.send().then(res => {
+	console.log(res.body);
+}).catch(err => {
+	console.log(err);
+});
